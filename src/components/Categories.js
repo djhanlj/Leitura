2cfed94bb5957e3242f53dc6b2991c4559511c06
djair <djhanlj@gmail.com>
2018-10-28 22:31:13 -0300

    implementação do menu

---
+++ src/components/Categories.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Jumbotron, Grid } from 'react-bootstrap';
+
+
+export default class Categories extends Component {
+  render() {
+    return (
+      <Grid>
+        <Jumbotron>
+          <h2>Categories</h2>
+        </Jumbotron>
+      </Grid>
+    )
+  }
+}
---
+++ src/utils/LeituraAPI.js
@@ -0,0 +1,14 @@
+
+const api = "http://localhost:3001"
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': 'whatever-you-want'
+}
+
+
+export const getCategories = () =>
+  fetch(`${api}/categories`, { headers })
+    .then(res => res.json())
+    .then(data => data.categories)
+
